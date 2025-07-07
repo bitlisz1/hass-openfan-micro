0aa08cd869d85bbc0bc0a51a76ba9f2e9db0c749
Jens Langhammer <jens.langhammer@beryju.org>
2025-07-08 00:39:26 +0200

    simplify setup

--- custom_components/openfan_micro/__init__.py
+++ custom_components/openfan_micro/__init__.py
@@ -1,16 +1,27 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
+from .binary_sensor import OpenFANMicroStallSensor
 from .const import DOMAIN, PLATFORMS
+from .fan import OpenFANMicroEntity
+from .sensor import OpenFANMicroRPMSensor
 
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+async def async_setup_entry(
+    hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
+):
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
 
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    host = entry.data["host"]
+    name = entry.data.get("name")
 
-    return True
+    fan_entity = OpenFANMicroEntity(host, name)
+    rpm_sensor = OpenFANMicroRPMSensor(host, name)
+    stall_sensor = OpenFANMicroStallSensor(name, fan_entity, rpm_sensor)
+
+    async_add_entities([fan_entity, rpm_sensor, stall_sensor])
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
