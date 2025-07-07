fd93369403409622643fbd70c6151ced2eaf035e
Jens Langhammer <jens.langhammer@beryju.org>
2025-07-07 21:36:45 +0200

    fix

--- custom_components/openfan_micro/__init__.py
+++ custom_components/openfan_micro/__init__.py
@@ -3,14 +3,18 @@ from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
 
+
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
 
-    await hass.config_entries.async_forward_entry_setup(entry, "fan")
+    await hass.config_entries.async_forward_entry_setups(entry, ["fan"])
+
     return True
 
+
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    await hass.config_entries.async_forward_entry_unload(entry, "fan")
-    hass.data[DOMAIN].pop(entry.entry_id)
-    return True
+    unload_ok = await hass.config_entries.async_forward_entry_unload(entry, "fan")
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return unload_ok
